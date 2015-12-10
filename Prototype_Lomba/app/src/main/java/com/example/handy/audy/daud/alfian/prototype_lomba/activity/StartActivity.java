2dbd2fedce0670f71f02c4f73e1964fb587da9f6
xalvsx <alfian.setyo17@gmail.com>
2015-12-10 22:49:19 +0700

    + v0.9

--- Prototype_Lomba/app/src/main/java/com/example/handy/audy/daud/alfian/prototype_lomba/activity/StartActivity.java
+++ Prototype_Lomba/app/src/main/java/com/example/handy/audy/daud/alfian/prototype_lomba/activity/StartActivity.java
@@ -1,7 +1,15 @@
 package com.example.handy.audy.daud.alfian.prototype_lomba.activity;
 
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcF;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -9,6 +17,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -26,7 +35,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StartActivity extends BaseActivity {
@@ -35,6 +46,11 @@ public class StartActivity extends BaseActivity {
     EditText txtIdKtp;
     Button btnLogin;
     String idKtp,idUser;
+
+    NfcAdapter nfc;
+    String[][] techListsArray;
+    IntentFilter[] intentFiltersArray;
+    PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +74,7 @@ public class StartActivity extends BaseActivity {
 
         txtIdKtp = (EditText)findViewById(R.id.txtIdKtp);
 
+        //nanti hapus
         btnLogin = (Button)findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -67,7 +84,109 @@ public class StartActivity extends BaseActivity {
                 new CheckKtp().execute();
             }
         });
+        //nanti hapus
 
+        nfc = NfcAdapter.getDefaultAdapter(this);
+
+        /* nanti buka
+        if (nfc == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }*/
+
+        pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndef.addDataType("text/plain");
+        }
+        catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+        intentFiltersArray = new IntentFilter[] {ndef };
+
+        techListsArray = new String[][] { new String[] { NfcF.class.getName() } };
+
+    }
+
+    private void readIdKtp() {
+        idKtp = txtIdKtp.getText().toString();
+        new CheckKtp().execute();
+    }
+
+    public void onPause() {
+        super.onPause();
+        nfc.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfc.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
+    }
+
+    public void onNewIntent(Intent intent) {
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        new NdefReaderTask().execute(tagFromIntent);
+        //do something with tagFromIntent
+    }
+
+    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+
+        @Override
+        protected String doInBackground(Tag... params) {
+            try {
+                Tag tag = params[0];
+
+                Ndef ndef = Ndef.get(tag);
+                if (ndef == null) {
+
+                    // NDEF is not supported by this Tag.
+                    return null;
+                }
+                NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+                NdefRecord[] records = ndefMessage.getRecords();
+                for (NdefRecord ndefRecord : records) {
+                    if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                        try {
+                            return readText(ndefRecord);
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+            byte[] payload = record.getPayload();
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+            int languageCodeLength = payload[0] & 0063;
+
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                if(result.equals("kosong")) {
+                    Toast.makeText(StartActivity.this, "Data kosong", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    txtIdKtp.setText(result);
+                    readIdKtp();
+                }
+            }
+        }
     }
 
     class CheckKtp extends AsyncTask<String,String,String> {
--- Prototype_Lomba/app/src/main/java/com/example/handy/audy/daud/alfian/prototype_lomba/activity/Voting.java
+++ Prototype_Lomba/app/src/main/java/com/example/handy/audy/daud/alfian/prototype_lomba/activity/Voting.java
@@ -1,9 +1,17 @@
 package com.example.handy.audy.daud.alfian.prototype_lomba.activity;
 
 import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcF;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -34,7 +42,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -46,6 +56,7 @@ public class Voting extends BaseActivity {
     Button btnKembali, btnKirim;
     List<HashMap<String,String>> pilihanVoting;
     RadioButton[] radioButtons;
+    EditText txtIdKtp;
 
     String idPertanyaan, soal, idUser, idKtp;
 
@@ -66,6 +77,13 @@ public class Voting extends BaseActivity {
 
     private int selectedId;
 
+    DialogPlus dialogp;
+
+    NfcAdapter nfc;
+    String[][] techListsArray;
+    IntentFilter[] intentFiltersArray;
+    PendingIntent pendingIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,8 +103,158 @@ public class Voting extends BaseActivity {
 
         new LoadPertanyaan().execute();
 
+        nfc = NfcAdapter.getDefaultAdapter(this);
 
+        /* nanti buka
+        if (nfc == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }*/
 
+        pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndef.addDataType("text/plain");
+        }
+        catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+        intentFiltersArray = new IntentFilter[] {ndef };
+
+        techListsArray = new String[][] { new String[] { NfcF.class.getName() } };
+
+        LinearLayout lay = new LinearLayout(Voting.this);
+        lay.setOrientation(LinearLayout.VERTICAL);
+        lay.setGravity(Gravity.CENTER);
+        LinearLayout.LayoutParams l = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        lay.setLayoutParams(l);
+        TextView tvTitle = new TextView(Voting.this);
+        tvTitle.setTextSize(20);
+        tvTitle.setText("Tempel kembali e-ktp anda");
+        lay.addView(tvTitle);
+        txtIdKtp = new EditText(Voting.this);
+        txtIdKtp.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        //txtIdKtp.setEnabled(false);
+        txtIdKtp.setSingleLine(true);
+        lay.addView(txtIdKtp);
+
+        final EditText txtIdKtp2 = txtIdKtp;
+
+        Button btnVerifikasi = new Button(Voting.this);
+        btnVerifikasi.setText("Verifikasi");
+        btnVerifikasi.setGravity(Gravity.CENTER);
+        lay.addView(btnVerifikasi);
+
+        btnVerifikasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String idKtp2 = txtIdKtp.getText().toString();
+                if (idKtp.equals(idKtp2)) {
+                    dialogp.dismiss();
+                    new SubmitJawaban().execute();
+                }
+                else {
+                    Toast.makeText(Voting.this, "Data E-KTP tidak valid", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        dialogp = DialogPlus.newDialog(Voting.this)
+                .setContentHolder(new ViewHolder(lay))
+                .setGravity(Gravity.CENTER)
+                .setPadding(25, 25, 25, 25)
+                .create();
+
+    }
+
+    private void readIdKtp() {
+        selectedId = rg.getCheckedRadioButtonId();
+
+        if (selectedId != -1 && dialogp.isShowing()) {
+            String idKtp2 = txtIdKtp.getText().toString();
+            if (idKtp.equals(idKtp2)) {
+                dialogp.dismiss();
+                new SubmitJawaban().execute();
+            } else {
+                Toast.makeText(Voting.this, "Data E-KTP tidak valid", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void onPause() {
+        super.onPause();
+        nfc.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfc.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
+    }
+
+    public void onNewIntent(Intent intent) {
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        new NdefReaderTask().execute(tagFromIntent);
+        //do something with tagFromIntent
+    }
+
+    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+
+        @Override
+        protected String doInBackground(Tag... params) {
+            try {
+                Tag tag = params[0];
+
+                Ndef ndef = Ndef.get(tag);
+                if (ndef == null) {
+
+                    // NDEF is not supported by this Tag.
+                    return null;
+                }
+                NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+                NdefRecord[] records = ndefMessage.getRecords();
+                for (NdefRecord ndefRecord : records) {
+                    if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                        try {
+                            return readText(ndefRecord);
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+            byte[] payload = record.getPayload();
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+            int languageCodeLength = payload[0] & 0063;
+
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                if(result.equals("kosong")) {
+                    Toast.makeText(Voting.this, "Data kosong", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    txtIdKtp.setText(result);
+                    readIdKtp();
+                }
+            }
+        }
     }
 
     class LoadPertanyaan extends AsyncTask<String, String, String>
@@ -233,47 +401,6 @@ public class Voting extends BaseActivity {
                             .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
-                                    LinearLayout lay = new LinearLayout(Voting.this);
-                                    lay.setOrientation(LinearLayout.VERTICAL);
-                                    lay.setGravity(Gravity.CENTER);
-                                    LinearLayout.LayoutParams l = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-                                    lay.setLayoutParams(l);
-                                    TextView tvTitle = new TextView(Voting.this);
-                                    tvTitle.setTextSize(20);
-                                    tvTitle.setText("Tempel kembali e-ktp anda");
-                                    lay.addView(tvTitle);
-                                    final EditText txtIdKtp = new EditText(Voting.this);
-                                    txtIdKtp.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                                    //txtIdKtp.setEnabled(false);
-                                    txtIdKtp.setSingleLine(true);
-                                    lay.addView(txtIdKtp);
-
-                                    final EditText txtIdKtp2 = txtIdKtp;
-
-                                    Button btnVerifikasi = new Button(Voting.this);
-                                    btnVerifikasi.setText("Verifikasi");
-                                    btnVerifikasi.setGravity(Gravity.CENTER);
-                                    lay.addView(btnVerifikasi);
-
-                                    final DialogPlus dialogp = DialogPlus.newDialog(Voting.this)
-                                            .setContentHolder(new ViewHolder(lay))
-                                            .setGravity(Gravity.CENTER)
-                                            .setPadding(25, 25, 25, 25)
-                                            .create();
-
-                                    btnVerifikasi.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            String idKtp2 = txtIdKtp.getText().toString();
-                                            if (idKtp.equals(idKtp2)) {
-                                                dialogp.dismiss();
-                                                new SubmitJawaban().execute();
-                                            }
-                                            else {
-                                                Toast.makeText(Voting.this, "Data E-KTP tidak valid", Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                    });
                                     dialogp.show();
                                 }
                             })
